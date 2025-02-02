9d37bf6c5705e161ac99f6ca2652c6708fd0a716
john-v-benny <johnvbenny@gmail.com>
2025-02-02 16:21:41 +0530

    Added initial front-end of the project , which includes the login and the landing pages

---
+++ Front-end/src/Sign_in.jsx
@@ -0,0 +1,47 @@
+import React,{useState} from "react";
+import "./signin.css";
+import logo_cc from "./assets/logo_cc.png";
+import {useNavigate} from "react-router-dom";
+
+const Sign_in = () =>{
+
+    const[email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        navigate("/Landing")
+
+    }
+
+    return(
+        <div className="login-box">
+            <img src={logo_cc} alt="Logo" />
+            <form onSubmit={handleSignIn}>
+                <input className="i1"
+                type="email"
+                value={email}
+                placeholder="email"
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+                <input className="i2"
+                type="password"
+                value={password}
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <p className="fp">FORGOT PASSWORD?</p>
+                <button className="loginb" type="submit">SIGN IN</button>
+                <p>OR</p>
+                <button className="signupb">SIGN UP</button>
+            </form>
+            
+
+        </div>
+    )
+
+}
+
+export default Sign_in;
