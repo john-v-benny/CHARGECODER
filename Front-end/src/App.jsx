163418b66ac19792c90192dc673fde88f1510a7e
Aswini <aswiniksaswini47@gmail.com>
2025-03-25 22:42:58 +0530

    home page connection

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
 import SignIn from './SignIn';
 import Signup from './Signup';
 import Landing from './Landing';
@@ -10,7 +11,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/profile" element={<Profile />} />
